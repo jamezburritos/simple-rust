374eaa19d4da0fcf7f3c7e671a02ea1b02d8aca8
James Barret <jamesbarret715@gmail.com>
2023-01-16 22:26:02 +0200

    finished linkedlist

--- linkedlist/src/lib.rs
+++ linkedlist/src/lib.rs
@@ -1,20 +1,20 @@
-pub struct List {
-    head: Link
+pub struct List<T> {
+    head: Link<T>
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-    elem: i32,
-    next: Link
+struct Node<T> {
+    elem: T,
+    next: Link<T>
 }
 
-impl List { 
+impl<T> List<T> { 
     pub fn new() -> Self {
         List { head: None }
     }
 
-    pub fn push(&mut self, value: i32) {
+    pub fn push(&mut self, value: T) {
         let new_node = Box::new(Node {
             elem: value,
             next: self.head.take()
@@ -23,18 +23,82 @@ impl List {
         self.head = Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
-        match self.head.take() {
-            None => None, 
-            Some(node) => {
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
     }
 }
 
-impl Drop for List {
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T; 
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>
+}
+
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> Iter<'_, T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &node.elem
+        })
+    }
+}
+
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut link = self.head.take();
         while let Some(mut node) = link {
--- linkedlist/src/main.rs
+++ linkedlist/src/main.rs
@@ -1,13 +1,13 @@
 use linkedlist::List;
 
 fn main() {
-    let mut test_list = List::new();
+    let mut test_list: List<i32> = List::new();
     test_list.push(13);
     test_list.push(54);
     test_list.push(4);
     test_list.push(101);
 
-    while let Some(value) = test_list.pop() {
-        println!("{value}");
+    for item in test_list.iter() {
+        println!("{item}");
     }
 }
